824fb86c46588c5cbb9f872b4e059c79753393b1
agusTrih <ant3dong@gmail.com>
2020-07-15 15:15:20 +0700

    tes

--- routes/users/index.js
+++ routes/users/index.js
@@ -7,7 +7,7 @@ const { update } = require("../../models/User");
 // router.get("/", routes.getRegistration);
 router.post("/", routes.userRegistration);
 router.get("/", routes.getAllUsers);
-router.get("/", routes.getUser);
+router.get("/:id", routes.getUser);
 router.delete("/:id", routes.deleteUser);
 router.put("/:id", routes.editUser);
 // router.put("/", routes.updateRegistration);

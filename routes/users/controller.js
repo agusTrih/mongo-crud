3c008183487a61f4bf5e5f19f1cbdb240d48a36b
agusTrih <ant3dong@gmail.com>
2020-07-15 15:02:35 +0700

    add delete in controller

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -48,6 +48,23 @@ const routes = {
             res.send(error);
         }
     },
+    // delete user
+    deleteUser: async (req, res) => {
+        const _id = req.params.id;
+
+        try {
+            const deletedUser = await User.findByIdAndRemove({ _id });
+
+            res.send({
+                message: "1 user has been deleted",
+                deleted: deletedUser,
+            });
+        } catch (error) {
+            console.log(error);
+
+            res.send(error);
+        }
+    },
 };
 
 module.exports = routes;

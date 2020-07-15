61f66df2b82d70933676784b143ef3089587ab79
agusTrih <ant3dong@gmail.com>
2020-07-15 16:07:17 +0700

    push heroku

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { PORT } = require('./config');
+const { PORT } = require("./config");
 const localPort = PORT || 5000;
-const { db } = require('./config');
+const { db } = require("./config");
 
 app.use(express.json()); // body-parser
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Welcome to mongoose demo');
+app.get("/", (req, res) => {
+    res.send("Welcome in the morning");
 });
-app.use('/api/users', require('./routes/users'));
+app.use("/api/users", require("./routes/users"));
 
 if (db) {
     console.log(`Connected to database`);

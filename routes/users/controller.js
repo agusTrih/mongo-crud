732fd7d85ba6e25798a45dad857c16a7d972e39c
agusTrih <ant3dong@gmail.com>
2020-07-15 14:50:38 +0700

    add get all user

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -19,24 +19,19 @@ const routes = {
             res.send(error);
         }
     },
-    // getRegistration: async (req, res) => {
-    //     const { name, email, password } = req.body;
+    getAllUsers: async (req, res) => {
+        try {
+            const users = await User.find();
+            res.send({
+                message: "get all Users",
+                results: users,
+            });
+        } catch (error) {
+            console.log(error);
 
-    //     try {
-    //         const user = await User.create({
-    //             name,
-    //             email,
-    //             password,
-    //         });
-
-    //         res.send({
-    //             message: `Registration complete`,
-    //             result: user,
-    //         });
-    //     } catch (error) {
-    //         res.send(error);
-    //     }
-    // },
+            res.send(error);
+        }
+    },
 };
 
 module.exports = routes;
